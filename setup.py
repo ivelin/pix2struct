8a0182c2d45106f73ec3cba9eaf0ab2a82a360a4
ivelin <ivelin.eth@gmail.com>
2022-12-31 23:13:32 +0000

    build: fix setup.py for dataflow
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setuptools.setup(
     name="pix2struct",
     version="0.0.1",
     packages=setuptools.find_packages(),
-    install_requires=["tensorflow>=2.9"],
+    install_requires=["tensorflow>=2.9",
+                      "Pillow>=9.3"],
     extras_require={
         "dev": [
             "pytest",
